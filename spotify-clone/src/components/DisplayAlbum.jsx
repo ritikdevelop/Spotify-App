8221c45b4d4f28408207b89940636cd3ca451717
ritikdevelop <hrithikrana216@gmail.com>
2024-08-13 18:14:01 +0530

    New comment in spotify-clone-app

--- spotify-clone/src/components/DisplayAlbum.jsx
+++ spotify-clone/src/components/DisplayAlbum.jsx
@@ -38,18 +38,14 @@ const DisplayAlbum = () => {
         <img className="m-auto w-4" src={assets.clock_icon} alt="" />
       </div>
       <hr />
-      {songsData.map((item,index) => (
+      {songsData.map((item, index) => (
         <div
           key={index}
           className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff26] cursor-pointer"
         >
           <p className="text-white">
             <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
-            <img
-              className="inline w-10 mr-5"
-              src={item.image}
-              alt=""
-            />
+            <img className="inline w-10 mr-5" src={item.image} alt="" />
             {item.name}
           </p>
           <p className="text-[15px]">{albumData.name}</p>
--- spotify-clone/src/components/Navbar.jsx
+++ spotify-clone/src/components/Navbar.jsx
@@ -3,19 +3,20 @@ import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-
-  const navigate = useNavigate();
+const navigate = useNavigate();
 
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
-          <img onClick={()=>navigate(-1)}
+          <img
+            onClick={() => navigate(-1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_left}
             alt=""
           />
-          <img onClick={()=>navigate(1)}
+          <img
+            onClick={() => navigate(1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_right}
             alt=""
@@ -36,9 +37,13 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center gap-2 mt-4">
-        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">All</p>
+        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">
+          All
+        </p>
         <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
-        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Podcasts</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">
+          Podcasts
+        </p>
       </div>
     </>
   );
