02ab7173811a29f12c3331cdea4651f757e2c8d7
ritikdevelop <hrithikrana216@gmail.com>
2025-01-12 00:31:54 +0530

    `Reverted import order of mongoose in songModel.js`

--- spotify-clone/SPOTIFY-BACKEND/src/config/cloudinary.js
+++ spotify-clone/SPOTIFY-BACKEND/src/config/cloudinary.js
@@ -1,7 +1,7 @@
 import {v2 as cloudinary} from 'cloudinary';
 
 const connectCloudinary = async () =>{
-    await cloudinary.config({
+    cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_SECRET_KEY
